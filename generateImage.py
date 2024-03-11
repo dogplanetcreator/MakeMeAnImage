97cd9d7c9909b09352c5e3c881a291ba8b76bfaa
dogplanetcreator <162892453+dogplanetcreator@users.noreply.github.com>
2024-03-11 21:55:55 +0000

    error messages

--- generateImage.py
+++ generateImage.py
@@ -14,20 +14,34 @@ def generateImage(client, model_choice, prompt,imagequality_choice):
             # Display message about missing prompt
             st.write('Prompt missing')
         else:
-            # create the image generation request
-            response = client.images.generate(
-                model=model_choice,
-                prompt=prompt,
-                size="1024x1024",
-                quality=imagequality_choice,
-                n=1
-                )
-            image_url = response.data[0].url
-            print("Generated Image URL:", image_url)
+            try:
+                # create the image generation request
+                response = client.images.generate(
+                    model=model_choice,
+                    prompt=prompt,
+                    size="1024x1024",
+                    quality=imagequality_choice,
+                    n=1
+                    )
+            except openai.APIError as e:
+                #Handle API error here, e.g. retry or log
+                print(f"OpenAI API returned an API Error: {e}")
+                pass
+            except openai.APIConnectionError as e:
+                #Handle connection error here
+                print(f"Failed to connect to OpenAI API: {e}")
+                pass
+            except openai.RateLimitError as e:
+                #Handle rate limit error (we recommend using exponential backoff)
+                print(f"OpenAI API request exceeded rate limit: {e}")
+                pass
 
-            response = requests.get(image_url)
-            img = Image.open(BytesIO(response.content))
+                image_url = response.data[0].url
+                print("Generated Image URL:", image_url)
 
-            # Display the image
-            st.image(img)
-            
+                response = requests.get(image_url)
+                img = Image.open(BytesIO(response.content))
+
+                # Display the image
+                st.image(img)
+                
