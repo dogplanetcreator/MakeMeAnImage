06bf8039596ca16d9a69360d454f1bc4131bb34a
dogplanetcreator <162892453+dogplanetcreator@users.noreply.github.com>
2024-03-12 11:13:58 +0000

    error messages

--- main.py
+++ main.py
@@ -10,17 +10,5 @@ model_choice, prompt, api_key, imagequality_choice,imagesize_choice, imageamount
 client = OpenAI(api_key=api_key)
 
 # generate image and display it
-try:
-     generateImage(client=client, model_choice=model_choice, prompt=prompt,imagequality_choice=imagequality_choice, imagesize_choice=imagesize_choice, imageamount_choice=imageamount_choice)
-except openai.APIError as e:
-    #Handle API error here, e.g. retry or log
-    print(f"OpenAI API returned an API Error: {e}")
-    pass
-except openai.APIConnectionError as e:
-    #Handle connection error here
-    print(f"Failed to connect to OpenAI API: {e}")
-    pass
-except openai.RateLimitError as e:
-    #Handle rate limit error (we recommend using exponential backoff)
-    print(f"OpenAI API request exceeded rate limit: {e}")
-    pass
+
+generateImage(client=client, model_choice=model_choice, prompt=prompt,imagequality_choice=imagequality_choice, imagesize_choice=imagesize_choice, imageamount_choice=imageamount_choice)
