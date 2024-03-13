6e830f412a64105462593a2fe14fa3c8fe53d636
dogplanetcreator <162892453+dogplanetcreator@users.noreply.github.com>
2024-03-13 11:04:29 +0000

    test

--- generateImage.py
+++ generateImage.py
@@ -18,30 +18,57 @@ def generateImage(client, model_choice, prompt,imagequality_choice,imagesize_cho
             st.write('Prompt missing')
         else:
 
-            for n in range(1, imageamount_choice+1):
+            #for a in range(1, imageamount_choice+1):
 
-                # create the image generation request
-                response = client.images.generate(
-                    model=model_choice,
-                    prompt=prompt,
-                    size=imagesize_choice,
-                    quality=imagequality_choice,
-                    n=imageamount_choice
-                    )
-           
+                 try:
+                     # create the image generation request
+                     response = client.images.generate(
+                     model=model_choice,
+                     prompt=prompt,
+                     size=imagesize_choice,
+                     quality=imagequality_choice,
+                     n=imageamount_choice
+                 )
+                 except openai.RateLimitError as e:
+                  #Handle rate limit error (we recommend using exponential backoff)
+                     #delay next try
+                     time.sleep(120)
+                     try:
+                         # create the image generation request
+                         response = client.images.generate(
+                         model=model_choice,
+                         prompt=prompt,
+                         size=imagesize_choice,
+                         quality=imagequality_choice,
+                         n=imageamount_choice
+                     )
+                     except openai.RateLimitError as e:
+                      #Handle rate limit error (we recommend using exponential backoff)      
+                      print(f"OpenAI API request exceeded rate limit: {e}")
+                      pass
+                     except openai.APIError as e:
+                      #Handle API error here, e.g. retry or log
+                      print(f"OpenAI API returned an API Error: {e}")
+                      pass
+                     except openai.APIConnectionError as e:
+                        #Handle connection error here
+                        print(f"Failed to connect to OpenAI API: {e}")
+                        pass
+                     except openai.ServiceUnavailableError as e:
+                      # Handle 5xx errors here
+                      print(f"OpenAI API request error: {e}")
+                      pass
+                 
+
                 
-                image_url = response.data[0].url
-                print("Generated Image URL:", image_url)
+                 image_url = response.data[0].url
+                 print("Generated Image URL:", image_url)
 
-                response = requests.get(image_url)
-                img = Image.open(BytesIO(response.content))
-
-                # Display the image
-                st.image(img)
                 
-                if imageamount_choice<3:
-                    # adding 20 seconds time delay
-                    time.sleep(20)
-                else:
-                    # adding 120 seconds time delay
-                    time.sleep(120) 
+                 response = requests.get(image_url)
+                 img = Image.open(BytesIO(response.content))
+
+                 # Display the image
+                 st.image(img)
+                    
+                 
