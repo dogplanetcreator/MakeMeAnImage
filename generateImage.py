9beb8122b7109f4e8579dc2a65d40fc16c5be4b1
dogplanetcreator <162892453+dogplanetcreator@users.noreply.github.com>
2024-03-13 15:06:41 +0000

    test

--- generateImage.py
+++ generateImage.py
@@ -85,6 +85,6 @@ def generateImage(client, model_choice, prompt,imagequality_choice,imagesize_cho
                     #Save the image
                     imagesavepathandname=str(str(imagesavepath) + str(imagesaveprefix) + str(a) + ".jpg")  
                     st.write(imagesavepathandname)
-                    img.save(imagesavepathandname)
+                    #img.save(imagesavepathandname)
                     
                  
