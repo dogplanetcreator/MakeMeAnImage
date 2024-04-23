7447a7c4b6b2d156ccc57658e6349284b75c350a
dogplanetcreator <162892453+dogplanetcreator@users.noreply.github.com>
2024-04-23 11:41:27 +0000

    te

--- input.py
+++ input.py
@@ -7,24 +7,12 @@ def takeInput():
     # Ask for the API key
     api_key = st.text_input("Enter your OpenAI API key:", type="password")
 
-    # Ask for the model choice
-    model_choice = st.selectbox(
-        "Which Dall E model would you like to use? ",
-        ("DALL·E 3")
-        index=0,
-        
-    )
-    # Display user choice
-    st.write('You selected:', model_choice)
-    
- 
- 
-    # Logic if model is selected
-    if model_choice == "DALL·E 3":
-        model_choice = "dall-e-3"
+    model_choice="dall-e-3"
+
+  
 
          # Ask for the quality of images Dall E 3
-        imagequality_choice = st.selectbox(
+    imagequality_choice = st.selectbox(
             "Quality of images?",
             ("standard","hd"),
             index=0,
@@ -32,18 +20,18 @@ def takeInput():
         )
 
         # Ask for the size of images Dall E 3
-        imagesize_choice = st.selectbox(
-            "Size of images?",
-            ("1024x1024","1792x1024","1024x1792"),
-            index=0,
+    imagesize_choice = st.selectbox(
+        "Size of images?",
+        ("1024x1024","1792x1024","1024x1792"),
+        index=0,
           
         )
         
          # Ask for the number of images
-        imageamount_choice = st.selectbox(
-            "How many images would you like to create? ",
-            (1,2,3,4,5,6,7,8,9,10),
-            index=0,
+    imageamount_choice = st.selectbox(
+        "How many images would you like to create? ",
+        (1,2,3,4,5,6,7,8,9,10),
+        index=0,
           
         )
 
@@ -74,36 +62,12 @@ def takeInput():
         #st.write('You selected image size:', imagesize_choice)
         #st.write('You selected image amount:', imageamount_choice)
 
-    else:
-        model_choice = "dall-e-2"
-
-        
-        # Image quality can be only standard
-        imagequality_choice="standard"
-        st.write('You can only use quality:', imagequality_choice)
-
-        # Ask for the size of images Dall E 2
-        imagesize_choice = st.selectbox(
-            "Size of images?",
-            ("256x256", "512x512","1024x1024"),
-            index=2,
-          
-        )
-        # Display user choice
-        st.write('You selected:', imagesize_choice)
-
-        # Ask for the number of images
-        imageamount_choice = st.selectbox(
-            "How many images would you like to create? ",
-            (1,2,3,4,5,6,7,8,9,10),
-            index=0,
+   
             
-        )
-        # Display user choice
-        st.write('You selected:', model_choice)
-        st.write('You selected image quality:', imagequality_choice)
-        st.write('You selected image size:', imagesize_choice)
-        st.write('You selected image amount:', imageamount_choice)
+
+    st.write('You selected image quality:', imagequality_choice)
+    st.write('You selected image size:', imagesize_choice)
+    st.write('You selected image amount:', imageamount_choice)
 
 
     # Ask if images shall be saved locally
